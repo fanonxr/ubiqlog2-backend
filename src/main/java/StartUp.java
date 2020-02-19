71fb11ccc52495c8a81e5eca9234649248797048
FanonX Rogers <fanonxdevelops@gmail.com>
2020-02-19 11:38:26 -0500

    Project Setup: initial project configuration.

---
+++ src/main/java/StartUp.java
@@ -0,0 +1,10 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StartUp {
+  private static final Logger logger = LoggerFactory.getLogger("StartUp");
+  public static void main(String[] args) {
+    logger.debug("Testing run");
+
+  }
+}
